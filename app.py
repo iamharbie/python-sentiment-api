f0d532acb0e66222ac93cf785f99df58bdb96a84
Pranav Bokey <pbokey@gatech.edu>
2017-07-21 15:23:44 -0400

    added test

--- app.py
+++ app.py
@@ -6,21 +6,19 @@ import json
 import requests
 from flask import Flask, request, render_template, jsonify
 
+sys.path.insert(0, "sentiment/sentiment.py")
+import sentiment as se
+
 app = Flask(__name__)
 
-
+# this is the index get method that displays a template that shows at the main heroku webpage
 @app.route('/', methods=['GET'])
 def root():
 	return render_template('index.html')
 
+# no POST methods in this application
 @app.route('/', methods=['POST'])
 def webhook():
-
-	# endpoint for processing incoming messaging events
-
-	data = request.get_json()
-	# log(data)
-	message_to_send = data['number']
 	return "ok", 200
 
 @app.route('/getSentiment', methods=['GET'])
@@ -29,6 +27,8 @@ def sentiment():
 	# endpoint for processing incoming messaging events
 
 	data = request.get_json()
+	data = data.dumps()
+	print(data)
 	# log(data)
 	return jsonify({'message' : 'it works'})
 
