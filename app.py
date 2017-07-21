2692cb7554da4c5aae2e3cb83ff8f9a0d2495adf
Pranav Bokey <pbokey@gatech.edu>
2017-07-21 12:34:09 -0400

    renamed

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def root():
-	return render_tempelate('index.html')
+	return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def webhook():
