7f64c76296324b1386ed29f19b8b157cc6145aea
Pranav Bokey <pbokey@gmail.com>
2017-07-25 23:13:00 -0400

    modified test file

--- tests.py
+++ tests.py
@@ -2,10 +2,7 @@ import requests
 import json
 import re
 
-# r = requests.get("https://still-castle-73273.herokuapp.com/getSentiment", params={'keyword': 'tesla'})
-# print(r.text)
+r = requests.get("https://still-castle-73273.herokuapp.com/getSentiment", params={'keyword': 'Piers_Morgan'})
+print(r.text)
 
-x = "Piers_Morgan"
-x = x.replace("_", " ")
-print(x)
 
